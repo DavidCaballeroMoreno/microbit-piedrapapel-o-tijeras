c2ea3c24291d68772c5d48ef8296b550100ba9ba
DavidCaballeroMoreno <98742614+DavidCaballeroMoreno@users.noreply.github.com>
2022-02-07 11:29:21 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let Jugada = 0
+input.onGesture(Gesture.Shake, function () {
+    Jugada = randint(1, 3)
+    if (Jugada == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (Jugada == 2) {
+        basic.showLeds(`
+            . # . # .
+            . . # . .
+            . # . # .
+            # # . # #
+            # # . # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
